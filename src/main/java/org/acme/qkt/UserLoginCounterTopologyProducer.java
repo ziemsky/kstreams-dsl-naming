fb0bdcc6104dcc628b8feb40329c954f5bead930
Rafał Kociński <rk@ziemsky.com>
2023-11-21 14:49:00 +0000

    WIP.

--- src/main/java/org/acme/qkt/UserLoginCounterTopologyProducer.java
+++ src/main/java/org/acme/qkt/UserLoginCounterTopologyProducer.java
@@ -1,14 +1,16 @@
 package org.acme.qkt;
 
 import io.quarkus.kafka.client.serialization.ObjectMapperSerde;
-import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.acme.qkt.model.LoginCountEvent;
 import org.acme.qkt.model.LoginEvent;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.state.KeyValueStore;
 
 @ApplicationScoped
